8b1ec75bd600ceb2773b1369ad1a2ac1dc852f24
tianyuStar <yunxing.liu@foxmail.com>
2018-07-13 13:36:16 +0800

    写websocket接口，但是具体发送的消息还需要完善

---
+++ src/main/java/com/example/demo/Config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yunxi
+ * Date: 2018/7/13
+ * Time: 9:15
+ * Description: No Description
+ */
+
+@Configuration
+public class WebSocketConfig{
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
